8d5a7f7219185e9c2ea59effb76229866857742c
Nikolay <nyk190@mail.ru>
2022-05-04 21:10:04 +0300

    Add changeMod function to script.js
    Add changeMod function to script.js, which change html class depend of
    clicked version in menu.

--- script.js
+++ script.js
@@ -10,3 +10,31 @@ function sliderMove() {
 }
 
 setInterval(sliderMove, 7000);
+
+let menuItems = document.querySelectorAll('.menu__item');
+menuItems.forEach(item => {
+    item.addEventListener('click', changeMod);
+});
+
+function changeMod(e) {
+    let root = document.documentElement;
+    let slider = document.querySelector('.slider');
+    slider.scroll(0, 0);
+    switch (true) {
+        case (this.classList.contains('menu__item_std')):
+            root.classList.remove('hearts-of-stone');
+            root.classList.remove('blood-and-wine');
+            root.classList.add('standard');
+            break;
+        case (this.classList.contains('menu__item_hos')):
+            root.classList.remove('standard');
+            root.classList.remove('blood-and-wine');
+            root.classList.add('hearts-of-stone');
+            break;
+        case (this.classList.contains('menu__item_baw')):
+            root.classList.remove('hearts-of-stone');
+            root.classList.remove('standard');
+            root.classList.add('blood-and-wine');
+            break;
+    }
+}
